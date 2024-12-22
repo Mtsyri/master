e7688a7397a881eff2900cfcd0cdeed22dc66098
Lodya <lodya@TDB.local>
2024-12-22 05:19:30 +0300

    Добавлен обработчик ошибок.

--- convertToBase.py
+++ convertToBase.py
@@ -1,10 +1,35 @@
-number = int(input("Введите число: "))
-numeralSystem = int(input("Введите основание системы счисления (например, 2 для двоичной, 8 для восьмеричной и т.д.): "))
-numberInNumeralSystem = ""
+def convertToBase(number, numeralSystem):
+    numberInNumeralSystem = ""
 
-while number > 0:
-    numberInNumeralSystem += str(number % numeralSystem)
-    number //= numeralSystem
+    while number > 0:
+        numberInNumeralSystem += str(number % numeralSystem)
+        number //= numeralSystem
 
-print("Число в новой системе счисления:", numberInNumeralSystem[::-1])
+    return numberInNumeralSystem[::-1]
+
+
+def getInput():
+    number = int(input("Введите число: "))
+    numeralSystem = int(
+        input("Введите основание системы счисления (например, 2 для двоичной, 8 для восьмеричной и т.д.): "))
+
+    if numeralSystem < 2 or numeralSystem > 36:
+        print("Ошибка: основание системы счисления должно быть в диапазоне от 2 до 36.")
+        return None, None
+
+    return number, numeralSystem
+
+
+def main():
+    number, numeralSystem = getInput()
+
+    if number is None or numeralSystem is None:
+        return
+
+    result = convertToBase(number, numeralSystem)
+    print("Число в новой системе счисления:", result)
+
+
+if __name__ == "__main__":
+    main()
 
