c70bf6b95c8a205396f0612a2fc3fce1b2140c23
Mtsyri <xxx@etripleX.com>
2024-12-25 05:50:52 +0300

    Second version of the project.

--- convertToBase.py
+++ convertToBase.py
@@ -1,9 +1,34 @@
-number = int(input("Enter the number: "))
-numeralSystem = int(input("Enter the base of the number system (for example, 2 for binary, 8 for octal, etc.): "))
-numberInNumeralSystem = ""
+def convertToBase(number, numeralSystem):
+    numberInNumeralSystem = ""
 
-while number > 0:
-    numberInNumeralSystem += str(number % numeralSystem)
-    number //= numeralSystem
+    while number > 0:
+        numberInNumeralSystem += str(number % numeralSystem)
+        number //= numeralSystem
 
-print(numberInNumeralSystem[::-1])
+    return numberInNumeralSystem[::-1]
+
+
+def getInput():
+    number = int(input("Enter number: "))
+    numeralSystem = int(
+        input("Enter the base of the number system (for example, 2 for binary, 8 for octal, etc.): "))
+
+    if numeralSystem < 2 or numeralSystem > 36:
+        print("Error: the base of the number system should be in the range from 2 to 36.")
+        return None, None
+
+    return number, numeralSystem
+
+
+def main():
+    number, numeralSystem = getInput()
+
+    if number is None or numeralSystem is None:
+        return
+
+    result = convertToBase(number, numeralSystem)
+    print("The number in the new number system:", result)
+
+
+if __name__ == "__main__":
+    main()
